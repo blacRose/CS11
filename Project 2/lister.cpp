59f83543102ce3c32f0760bd2a0cfac7edc4f02e
Shelby Munsch <smunsch@gmail.com>
2015-02-24 08:37:13 -0800

    start on implementation

--- Project 2/lister.cpp
+++ Project 2/lister.cpp
@@ -1,9 +1,27 @@
 #include "lister.h"
 #include "list.h"
 
-void addToHead(std::string);
-void addToTail(std::string);
-void addWhere(std::string, size_t);
-void sortAdd(std::string);
-void printList() const;
-std::string get(size_t);
+void addToHead(const std::string&)
+{
+	
+}
+void addToTail(const std::string&)
+{
+	
+}
+void addWhere(const std::string&, size_t)
+{
+	
+}
+void sortAdd(const std::string&)
+{
+	
+}
+void printList() const
+{
+	
+}
+std::string get(size_t)
+{
+	
+}
--- Project 2/lister.h
+++ Project 2/lister.h
@@ -1,10 +1,10 @@
 class lister
 {
 public:
-	void addToHead(std::string);
-	void addToTail(std::string);
-	void addWhere(std::string, size_t);
-	void sortAdd(std::string);
+	void addToHead(const std::string&);
+	void addToTail(const std::string&);
+	void addWhere(const std::string&, size_t);
+	void sortAdd(const std::string&);
 	void printList() const;
 	std::string get(size_t);
 private:
