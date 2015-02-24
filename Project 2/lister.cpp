b7ac5c8f2f197778829ce277c8fa8adcaef133da
Shelby Munsch <smunsch@gmail.com>
2015-02-24 08:29:09 -0800

    oops. inc that.

--- Project 2/lister.cpp
+++ Project 2/lister.cpp
@@ -1,1 +1,9 @@
 #include "lister.h"
+#include "list.h"
+
+void addToHead(std::string);
+void addToTail(std::string);
+void addWhere(std::string, size_t);
+void sortAdd(std::string);
+void printList() const;
+std::string get(size_t);
