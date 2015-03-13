1625ec07a4043bdefdd6300cb7210798b8523c36
Shelby Munsch <smunsch@gmail.com>
2015-03-13 13:33:25 -0700

    going to list<std::string>

--- Project 3/main.cpp
+++ Project 3/main.cpp
@@ -3,7 +3,7 @@
 *
 * MM, YUP!
 * Author: Shelby Munsch
-*/
+
 #include <iostream>
 #include "list.h"
 #include "lister.h"
@@ -12,4 +12,21 @@ int main(int argc, char const *argv[]) {
   lister lone, ltwo;
 
   return 0;
+}*/
+
+#include <iostream>
+#include "list.h"
+#include "lister.h"
+
+using namespace std;
+int main() {
+  string poo = "";
+  string inp;
+  lister lone;
+  lone.insert();
+  while (poo != "q") {
+    lone.movtob();
+    cout << "q to quit or anything to keep going: ";
+    cin >> poo;
+  }
 }
