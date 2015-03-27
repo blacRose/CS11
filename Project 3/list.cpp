5de412276def71cb0d848c85f55817a6d46d2f6e
Shelby Munsch <smunsch@gmail.com>
2015-03-27 11:25:13 -0700

    clean

--- Project 3/list.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include "list.h"
-
-template <class T>
-list<T>::list(T datas): data(datas), next(NULL) {};
-
-std::ostream &operator<<(std::ostream &os, const list<std::string> &l) { return os << l.data; }
