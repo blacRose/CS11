dc4e4a14c15bd0d989b69cf7249ca1dffc8289da
Shelby Munsch <smunsch@gmail.com>
2015-04-08 16:11:20 -0700

    getting linked

--- Project 4/main.cpp
+++ Project 4/main.cpp
@@ -6,14 +6,10 @@
 */
 
 #include <iostream>
-#include "list.h"
-#include "lister.h"
+#include "boundedstack.h"
+#include "boundedstacka.h"
+#include "boundedstackb.h"
 
 int main() {
   // nothing
 }
-
-// Create functions for your implementations that includes at a minimum the following functions. You decide which class or classes the functions should belong to.
-// - A function for adding an item to the top of the stack.
-// - A function for removing an item from the top of the stack.
-// - A function for displaying the item at the top of the stack.
