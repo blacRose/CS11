1d4597f22f984bd24c1823269a749b4a3fbfb9ab
Shelby Munsch <smunsch@gmail.com>
2015-04-10 22:48:53 -0700

    oops

--- Project 4/boundedstackb.cpp
+++ Project 4/boundedstackb.cpp
@@ -1,22 +1,23 @@
 #include <iostream>
 #include "boundedstack.h"
 
-// ordered starting at 50 (top)... stores from the back of the array
-// Handles overflow by pushing the first, oldest element (0) into nothing, then
-// shifting the other elements up, and throws the new element in 50.
+// ordered starting at 50 (top)... stores from the back of the array, up.
+// Handles overflow by pushing the first, oldest element (50) into nothing, then
+// shifting the other elements down, and throws the new element in top.
 // The newest element is at the value of top (when full, 1), going back in time to 50.
 
 
 void boundedstackb::push(std::string pdata)
-  {
-    if (top == 1) {
-      pop();
-    } else {
-      top--;
-      size++;
-    }
-    data[top] = pdata;
+{
+  if (top == 0) {
+    pop(top);
+    shift("down");
+  } else {
+    top--;
+    size++;
   }
+  data[top] = pdata;
+}
 std::string boundedstackb::pop() {
   //pulls newest element (data[top]) out and returns it.
   if (top == 50) {
