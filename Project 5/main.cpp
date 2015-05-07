a4bc03c48a1afbfc65683021490662226c6fcc18
Shelby Munsch <smunsch@gmail.com>
2015-05-07 07:56:10 -0700

    moving code

--- Project 5/main.cpp
+++ Project 5/main.cpp
@@ -12,51 +12,6 @@ using namespace std;
 
 #include "reversepoem.cpp"
 
-template <class Item>
-class reversePoem
-{
-public:
-  void input();
-  void sortPoem();
-  friend std::ostream &operator<<(std::ostream &, const reversePoem &);
-  void printPoem();
-private:
-  queue<Item> initialQ;
-  queue<Item> finalQ;
-};
-template <class Item>
-void reversePoem::sortPoem() {
-  queue<Item> tempQ = initialQ;
-  // Precondition: Two objects, initialQ and finalQ, of a queue class have been
-  // filled with data.
-  while (!initialQ.empty()) {
-    finalQ.emplace(tempQ.front());
-    tempQ.pop();
-  }
-  // Postcondition: The queue object initialQ has been loaded in reverse order
-  // onto the end of the queue object finalQ. In other words, finalQ contains
-  // its original data plus the reversed data from initialQ. The initialQ is
-  // empty.
-}
-template <class Item>
-ostream &reversePoem::operator>>(istream &eyes, const reversePoem &l)
-{
-  initialQ.emplace(eyes);
-}
-template <class Item>
-void reversePoem::printPoem() {
-  cout << "Your poem" << endl;
-  while (!finalQ.empty()) {
-    cout << finalQ.front() << endl;
-    finalQ.pop();
-  }
-}
-template <class Item>
-void reversePoem::input()
-{
-  cout << "Enter lines, or an empty line to quit" << endl;
-  cin >> initialQ;
-}
 int main() {
   char type;
   cout << "String (enter s), or char (c)?" << endl;
